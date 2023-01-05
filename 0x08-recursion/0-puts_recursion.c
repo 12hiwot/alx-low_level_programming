d59b1ba26b7a8fe31cc9b82aa602885382926554
12hiwot <hiwotyemane40@gmail.com>
2023-01-05 05:13:43 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,21 @@
 /*
  * file: 0-puts_recursion.c
- * Auth: sam
+ * Auth: hiwot
  */
 
 #include "main.h"
-
 /**
- * _puts_recursion -Prints astring, followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion - function like puts ();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-void _puts_recurision(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
 	else
-		_putchar('\n')
+		_putchar('\n');
 }
